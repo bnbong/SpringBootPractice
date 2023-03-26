9304006d8c36354d09ab611fc09aee5762bcd0a3
bnbong <bbbong9@gmail.com>
2023-03-26 13:47:19 +0900

    reinstall spring initializer

--- springbootpractice/src/main/java/com/example/springbootpractice/SpringbootpracticeApplication.java
+++ springbootpractice/src/main/java/com/example/springbootpractice/SpringbootpracticeApplication.java
@@ -1,13 +1,13 @@
-package com.example.SpringBootPractice;
+package com.example.springbootpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootPracticeApplication {
+public class SpringbootpracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPracticeApplication.class, args);
+		SpringApplication.run(SpringbootpracticeApplication.class, args);
 	}
 
 }
--- springbootpractice/src/test/java/com/example/springbootpractice/SpringbootpracticeApplicationTests.java
+++ springbootpractice/src/test/java/com/example/springbootpractice/SpringbootpracticeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.SpringBootPractice;
+package com.example.springbootpractice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootPracticeApplicationTests {
+class SpringbootpracticeApplicationTests {
 
 	@Test
 	void contextLoads() {

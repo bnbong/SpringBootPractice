15dc9dc14ce32483452809c5ced6f8a11d27ec34
bnbong <bbbong9@gmail.com>
2023-04-02 17:31:30 +0900

    learning get methods

---
+++ hello/src/main/java/com/springboot/hello/controller/GetController.java
@@ -0,0 +1,49 @@
+package com.springboot.hello.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+    // http://localhost:8080/api/v1/get-api/variable1/somevar
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        return variable;
+    }
+
+    // http://localhost:8080/api/v1/get-api/variable2/somevar
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable(value = "variable") String var) {
+        return var;
+    }
+
+    // http://localhost:8080/api/v1/get-api/request1?name=value1&email=value2&organization=value3
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(
+        @RequestParam String name,
+        @RequestParam String email,
+        @RequestParam String organization
+    ) {
+        return name + " " + email + " " + organization;
+    }
+
+    // http://localhost:8080/api/v1/get-api/request2?key1=value1&key2=value2
+    @GetMapping(value="/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param) {
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+    
+}
